6c499d49e3aed115f1b8dbe7e75504113f4356e6
Murkorus <95182180+Murkorus@users.noreply.github.com>
2024-04-17 11:49:08 +0200

    End of class push

--- RoomOfEscapingness/Assets/Scripts/WinTheGame.cs
+++ RoomOfEscapingness/Assets/Scripts/WinTheGame.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinTheGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image fadeToWhiteImage;
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            fadeToWhiteImage.CrossFadeAlpha(255, 3, true);
+        }
     }
 }
