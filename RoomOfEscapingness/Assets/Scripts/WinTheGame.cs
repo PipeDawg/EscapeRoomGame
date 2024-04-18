ff72d9e881bb873fcd27f46517cd402301ae3ad6
Murkorus <95182180+Murkorus@users.noreply.github.com>
2024-04-18 10:17:00 +0200

    End of Class Pyush

--- RoomOfEscapingness/Assets/Scripts/WinTheGame.cs
+++ RoomOfEscapingness/Assets/Scripts/WinTheGame.cs
@@ -1,16 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WinTheGame : MonoBehaviour
 {
     public Image fadeToWhiteImage;
-    private void Update()
+    public GameObject text;
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+
+    }
+
+    IEnumerator FadeTheAlpha(float targetAlpha)
+    {
+        Color tempColor = fadeToWhiteImage.color;
+        if(fadeToWhiteImage.color.a < targetAlpha)
         {
-            fadeToWhiteImage.CrossFadeAlpha(255, 3, true);
+            tempColor.a += 0.002f;
+            if(tempColor.a > 1)
+            {
+                tempColor.a = 1; // Safety Measure
+            }
         }
+        else if (fadeToWhiteImage.color.a > targetAlpha)
+        {
+            tempColor.a -= 0.002f;
+            if (tempColor.a < 0)
+            {
+                tempColor.a = 0; // Safety Measure
+            }
+        }
+        fadeToWhiteImage.color = tempColor;
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(FadeTheAlpha(targetAlpha));
+    }
+
+    IEnumerator ShowWinText()
+    {
+        yield return new WaitForSeconds(7);
+        text.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(FadeTheAlpha(1));
+        StartCoroutine(ShowWinText());
     }
 }
