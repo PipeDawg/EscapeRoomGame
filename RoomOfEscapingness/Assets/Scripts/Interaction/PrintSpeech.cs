b337616ac0a7264952faee166d53bdeee8c117b4
PipeDawg <Axel.ahrensbach@gmail.com>
2024-05-16 10:39:24 +0200

    printsound

--- RoomOfEscapingness/Assets/Scripts/Interaction/PrintSpeech.cs
+++ RoomOfEscapingness/Assets/Scripts/Interaction/PrintSpeech.cs
@@ -8,10 +8,12 @@ public class PrintSpeech : MonoBehaviour
     private InteractabbleBase interactable;
     public Animator animator;
     public GameObject speechPaperObject;
+    private AudioSource audioSource;
     private void Start()
     {
         //animator = GetComponentInChildren<Animator>();
         interactable = GetComponent<InteractabbleBase>();
+        audioSource = GetComponent<AudioSource>();
     }
     public void PrintSpeechPaper()
     {
@@ -20,6 +22,7 @@ public class PrintSpeech : MonoBehaviour
             speechPaperObject.SetActive(true);
             Debug.Log("Works");
             animator.SetBool("StartAnimation", true);
+            audioSource.Play();
         } else
         {
             Debug.Log("NO MORE PAPER :(");
